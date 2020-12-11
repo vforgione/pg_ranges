6fcfabc8e5a7b809a6f3d8c5c2923ad6686e76fa
Robert Parcus <robert@blockfi.com>
2020-12-11 18:54:43 +0800

    ♻️Refactor

--- lib/pg_ranges.ex
+++ lib/pg_ranges.ex
@@ -1,7 +1,7 @@
 defmodule PgRanges do
   @moduledoc """
   PgRanges provides a simple wrapper around `Postgrex.Range` so that you can
-  create scheams with range type fields and use the native range type in
+  create schemas with range type fields and use the native range type in
   migrations.
 
       defmodule MyApp.Employee do
@@ -38,4 +38,82 @@ defmodule PgRanges do
         where: fragment("? @> ?", e.employed_dates, type(^range, DateRange))
       )
   """
+
+  @callback new(lower :: any, upper :: any) :: any
+  @callback new(any, any, any) :: any
+  @callback from_postgrex(any) :: any
+  @callback to_postgrex(any) :: any
+
+  @optional_callbacks new: 2,
+                      new: 3,
+                      from_postgrex: 1,
+                      to_postgrex: 1
+
+  @doc false
+  defmacro __using__(opts) do
+    quote location: :keep, bind_quoted: [opts: opts] do
+      alias Postgrex.Range
+
+      use Ecto.Type
+      @behaviour PgRanges
+      @before_compile PgRanges
+
+      defstruct lower: nil,
+                lower_inclusive: true,
+                upper: nil,
+                upper_inclusive: false
+
+      @spec new(DateTime.t(), DateTime.t(), keyword()) :: __MODULE__.t()
+      def new(lower, upper, opts \\ []) do
+        fields = Keyword.merge(opts, lower: lower, upper: upper)
+
+        struct!(__MODULE__, fields)
+      end
+
+      @doc false
+      @spec from_postgrex(Range.t()) :: __MODULE__.t()
+      def from_postgrex(%Range{} = range), do: struct!(__MODULE__, Map.from_struct(range))
+
+      @doc false
+      @spec to_postgrex(__MODULE__.t()) :: Range.t()
+      def to_postgrex(%__MODULE__{} = range), do: struct!(Range, Map.from_struct(range))
+
+      @doc false
+      def cast(nil), do: {:ok, nil}
+      def cast(%Range{} = range), do: {:ok, from_postgrex(range)}
+      def cast(%__MODULE__{} = range), do: {:ok, range}
+      def cast(_), do: :error
+
+      @doc false
+      def load(nil), do: {:ok, nil}
+      def load(%Range{} = range), do: {:ok, from_postgrex(range)}
+      def load(_), do: :error
+
+      @doc false
+      def dump(nil), do: {:ok, nil}
+      def dump(%__MODULE__{} = range), do: {:ok, to_postgrex(range)}
+      def dump(_), do: :error
+
+      defoverridable new: 2,
+                     new: 3,
+                     from_postgrex: 1,
+                     to_postgrex: 1
+    end
+  end
+
+  defmacro __before_compile__(env) do
+    unless Module.defines?(env.module, {:type, 0}) do
+      message = """
+      function type/0 required by behaviour Ecto.Type is not implemented \
+      (in module #{inspect(env.module)}).
+      """
+
+      IO.warn(message, Macro.Env.stacktrace(env))
+
+      quote do
+        @doc false
+        def type, do: :not_a_valid_type
+      end
+    end
+  end
 end
--- lib/pg_ranges/daterange.ex
+++ lib/pg_ranges/daterange.ex
@@ -2,56 +2,15 @@ defmodule PgRanges.DateRange do
   @moduledoc """
   Wraps a `Postgrex.Range` and casts to a PostgreSQL `daterange` type.
   """
+  use PgRanges
 
-  defstruct r: nil
-
-  @type t :: %__MODULE__{r: Postgrex.Range.t()}
-
-  @doc """
-  Creates a new `PgRanges.DateRange` struct. It expects the _lower_ and _upper_
-  attributes to be `Date`s.
-
-  ## Options
-
-  - `lower_inclusive`: should the range be lower inclusive? Default is `true`
-  - `upper_inclusive`: should the range be upper inclusive? Default is `false`
-  """
-  @spec new(Date.t(), Date.t(), keyword()) :: PgRanges.DateRange.t()
-  def new(lower, upper, opts \\ []) do
-    fields =
-      [lower_inclusive: true, upper_inclusive: false]
-      |> Keyword.merge(opts)
-      |> Keyword.merge([lower: lower, upper: upper])
-
-    %PgRanges.DateRange{r: struct!(Postgrex.Range, fields)}
-  end
-
-  @doc false
-  @spec from_postgrex(Postgrex.Range.t()) :: PgRanges.DateRange.t()
-  def from_postgrex(%Postgrex.Range{} = r), do: %PgRanges.DateRange{r: r}
-
-  @doc false
-  @spec to_postgrex(PgRanges.DateRange.t()) :: Postgrex.Range.t()
-  def to_postgrex(%PgRanges.DateRange{r: r}), do: r
-
-  @behaviour Ecto.Type
+  @type t :: %__MODULE__{
+          lower: Date.t(),
+          lower_inclusive: integer(),
+          upper: Date.t(),
+          upper_inclusive: integer()
+        }
 
   @doc false
   def type, do: :daterange
-
-  @doc false
-  def cast(nil), do: {:ok, nil}
-  def cast(%Postgrex.Range{} = r), do: {:ok, from_postgrex(r)}
-  def cast(%PgRanges.DateRange{} = r), do: {:ok, r}
-  def cast(_), do: :error
-
-  @doc false
-  def load(nil), do: {:ok, nil}
-  def load(%Postgrex.Range{} = r), do: {:ok, from_postgrex(r)}
-  def load(_), do: :error
-
-  @doc false
-  def dump(nil), do: {:ok, nil}
-  def dump(%PgRanges.DateRange{} = r), do: {:ok, to_postgrex(r)}
-  def dump(_), do: :error
 end
