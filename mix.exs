36f0699b75dbe5c98d8e5c8515f146db9101cf4e
Vince Forgione <vforgione@users.noreply.github.com>
2024-01-06 22:32:18 -0600

    Update mix.exs
    Bumped version to 1.1.1 after merging PR

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PgRanges.MixProject do
   def project do
     [
       app: :pg_ranges,
-      version: "1.1.0",
+      version: "1.1.1",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
