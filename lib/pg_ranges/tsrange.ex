37a862a1967b5b8e730ee67070a12a099c8e31f9
Christopher Miller <32465602+camilleryr@users.noreply.github.com>
2024-03-05 08:52:11 -0600

    Override `equal?` for time based ranges

--- lib/pg_ranges/tsrange.ex
+++ lib/pg_ranges/tsrange.ex
@@ -12,6 +12,17 @@ defmodule PgRanges.TsRange do
         }
 
   @doc false
+  @impl true
   @spec type() :: :tsrange
   def type, do: :tsrange
+
+  @impl true
+  def equal?(%__MODULE__{} = left, %__MODULE__{} = right) do
+    DateTime.compare(left.lower, right.lower) == :eq and
+      left.lower_inclusive == right.lower_inclusive and
+      DateTime.compare(left.upper, right.upper) == :eq and
+      left.upper_inclusive == right.upper_inclusive
+  end
+
+  def equal?(_, _), do: false
 end
--- lib/pg_ranges/tstzrange.ex
+++ lib/pg_ranges/tstzrange.ex
@@ -36,4 +36,14 @@ defmodule PgRanges.TstzRange do
 
     struct!(__MODULE__, fields)
   end
+
+  @impl true
+  def equal?(%__MODULE__{} = left, %__MODULE__{} = right) do
+    DateTime.compare(left.lower, right.lower) == :eq and
+      left.lower_inclusive == right.lower_inclusive and
+      DateTime.compare(left.upper, right.upper) == :eq and
+      left.upper_inclusive == right.upper_inclusive
+  end
+
+  def equal?(_, _), do: false
 end
